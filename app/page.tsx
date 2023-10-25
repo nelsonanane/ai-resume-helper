738f8d4a1ebd9f0c80fe454d64bcad6fba412e00
nelsonanane <83245270+nelsonanane@users.noreply.github.com>
2023-10-24 21:44:37 -0400

    added window check

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-'use client'
-import React, {useRef} from "react";
+"use client";
+import React, { useRef } from "react";
 import Navigator from "./Navigator";
 import ResumeContainer from "./ResumeContainer";
-import { useReactToPrint } from 'react-to-print'; 
-
-
-
+import { useReactToPrint } from "react-to-print";
 
 export default function Home() {
   const componentRef = useRef<any>();
@@ -14,20 +11,22 @@ export default function Home() {
     content: () => componentRef.current,
   });
 
-  // const handleBeforeContent = () => { 
-  //   const img = document.getElementsByTagName("img")
-  //   console.log(img)
-   
-  //   document.getElementById("image-select") ? document.getElementById("image-select")!.style.display = "none" : '';
-  //     console.log('image', img)
-  //     console.log(document.getElementById("image-select"))
- 
-  // }
+  const handleBeforeContent = () => {
+    if (typeof window !== "undefined") {
+      const img = document.getElementsByTagName("img");
+      console.log(img);
+
+      document.getElementById("image-select")
+        ? (document.getElementById("image-select")!.style.display = "none")
+        : "";
+      console.log("image", img);
+      console.log(document.getElementById("image-select"));
+    }
+  };
 
   return (
-
     <div>
-      <Navigator onDownload={handleDownload}/>
+      <Navigator onDownload={handleDownload} />
       <ResumeContainer componentRef={componentRef} />
     </div>
   );

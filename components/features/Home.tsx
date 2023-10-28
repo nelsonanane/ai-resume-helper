3dc36684194a70c5dbb87dce0b0fcbb7c0710db2
nelsonanane <83245270+nelsonanane@users.noreply.github.com>
2023-10-27 20:49:40 -0400

    homepage updates

--- components/features/Home.tsx
+++ components/features/Home.tsx
@@ -1,14 +1,13 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import React from "react";
-import { Button } from "@/components/ui/button"; 
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 type Props = {};
 
-function Home({}: Props) { 
-
+function Home({}: Props) {
   return (
     <div className="relative">
       <Image
@@ -19,13 +18,16 @@ function Home({}: Props) {
         className="h-[calc(100vh-75px)] w-full -z-100 absolute"
         style={{ objectFit: "cover" }}
       />
-      <div className="absolute right-[100px] top-28 md:top-[400px]">
+      <div className="absolute right-[100px] top-[200px] md:top-[400px] w-[500px]">
         <h1 className="text-orange-50 text-7xl drop-shadow-[5px_5px_10px_#000000] body-text font-Font-Sans text-right">
           Quick Resume{" "}
         </h1>{" "}
         <div className="flex justify-between items-center ml-16">
-          <Button className="h-12 w-46 rounded-lg text-lg drop-shadow-[5px_5px_10px_#000000] mt-[100px] " asChild>
-            <Link href={'/resume'}>Start building now</Link>
+          <Button
+            className="h-12 w-46 rounded-lg text-lg drop-shadow-[5px_5px_10px_#000000] mt-[100px] "
+            asChild
+          >
+            <Link href={"/resume"}>Start building now</Link>
           </Button>
           <div className="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center ">
             <svg
@@ -45,6 +47,9 @@ function Home({}: Props) {
             Maker
           </h1>
         </div>
+        <p className="text-2xl leading-tight text-white m-12">
+        <strong>Time is Money:</strong> Have a polished resume in seconds, leaving you more time to prepare for interviews and network.
+        </p>
       </div>
     </div>
   );
